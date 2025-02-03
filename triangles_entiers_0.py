7af633c566850555492150ff09b687a8594b3416
Kilian Testard <pc47xiy@eduvaud.ch>
2025-02-03 11:54:19 +0100

    feat: brute method added for triangles_entiers ; smart method wip

--- triangles_entiers_0.py
+++ triangles_entiers_0.py
@@ -7,7 +7,7 @@ from tkinter import messagebox
 import time
 import math
 
-def find_square_harshad():
+def find_rect_triangle():
     try:
         # Récupérer les bornes min et max
         min_val = int(entry_min.get())
@@ -24,12 +24,13 @@ def find_square_harshad():
         # Trouver les triangles rectangles entiers
         # Solution brute
         for a in range(min_val, max_val + 1):
-            for b in range(min_val, max_val + 1):
-                c2 = a ** 2 + b ** 2
-                c = int(math.sqrt(c2))
-                if c ** 2 == c2:
-                    numbers_tested += 1
-                    solutions.append((a, b))
+            for b in range(a+1, max_val + 1):
+                if math.gcd(a, b) == 1:
+                    c2 = a ** 2 + b ** 2
+                    c = int(math.sqrt(c2))
+                    if int(math.sqrt(c2)) ** 2 == c2:
+                        numbers_tested += 1
+                        solutions.append((a, b, c))
 
 
         # Calcul du temps d'exécution
@@ -51,7 +52,7 @@ def find_square_harshad():
     except ValueError as e:
         messagebox.showerror("Erreur", str(e))
 
-def find_square_harshad_smart():
+def find_rect_triangle_smart():
     try:
         # Récupérer les bornes min et max
         min_val = int(entry_min.get())
@@ -67,6 +68,16 @@ def find_square_harshad_smart():
 
         # Trouver triangles rectangles entiers
         # solution smart
+        square = []
+        for c in range(min_val, int(max_val * math.sqrt(2))):
+            square.append(c ** 2)
+        print(square)
+        for a in range(min_val, max_val + 1):
+            for b in range(a + 1, max_val + 1):
+                if math.gcd(a, b) == 1:
+                    if a ** 2 + b ** 2 in square:
+                        numbers_tested += 1
+                        solutions.append((a, b, c))
 
 
         # Calcul du temps d'exécution
@@ -83,7 +94,7 @@ def find_square_harshad_smart():
             for sol in solutions:
                 text_output.insert(tk.END, f"{sol}\n")
         else:
-            text_output.insert(tk.END, "Aucun carré harshad trouvé.\n")
+            text_output.insert(tk.END, "Aucun triangle rectangle trouvé.\n")
 
     except ValueError as e:
         messagebox.showerror("Erreur", str(e))
@@ -101,15 +112,15 @@ entry_min.grid(row=0, column=1, padx=10, pady=5)
 tk.Label(root, text="Valeur maximale (max):").grid(row=1, column=0, padx=10, pady=5, sticky="e")
 entry_max = tk.Entry(root)
 entry_max.grid(row=1, column=1, padx=10, pady=5)
-entry_min.insert(0, "1000")  # Valeur par défaut pour min
-entry_max.insert(0, "1000000")  # Valeur par défaut pour max
+entry_min.insert(0, "1")  # Valeur par défaut pour min
+entry_max.insert(0, "1000")  # Valeur par défaut pour max
 
 # Boutons pour calculer
-btn_calculate = tk.Button(root, text="Triangles rect entiers", command=find_square_harshad)
+btn_calculate = tk.Button(root, text="Triangles rect entiers", command=find_rect_triangle)
 btn_calculate.grid(row=2, column=0,  pady=10)
-btn_calculate_smart = tk.Button(root, text="Triangles rect ent. smart", command=find_square_harshad_smart)
+btn_calculate_smart = tk.Button(root, text="Triangles rect ent. smart", command=find_rect_triangle_smart)
 btn_calculate_smart.grid(row=2, column=1,  pady=10)
-btn_calculate_prim = tk.Button(root, text="Triangles rect primitifs", command=find_square_harshad_smart)
+btn_calculate_prim = tk.Button(root, text="Triangles rect primitifs", command=find_rect_triangle_smart)
 btn_calculate_prim.grid(row=3, column=0,  pady=10)
 
 # Zone de texte pour afficher les résultats (dans un frame)
